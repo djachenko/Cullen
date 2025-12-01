f63afa6716bd8aebaf6ad9277c3d1e6e99740505
Igor Djachenko <i.s.djachenko@gmail.com>
2025-12-01 16:04:47 +0700

    chore: initial project setup

---
+++ Cullen/CullenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CullenApp.swift
+//  Cullen
+//
+//  Created by justin on 01.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CullenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CullenTests/CullenTests.swift
@@ -0,0 +1,17 @@
+//
+//  CullenTests.swift
+//  CullenTests
+//
+//  Created by justin on 01.12.2025.
+//
+
+import Testing
+@testable import Cullen
+
+struct CullenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
