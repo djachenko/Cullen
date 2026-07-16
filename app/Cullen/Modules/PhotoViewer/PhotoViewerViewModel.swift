86b0f66b285ab9b12881201af213c6f10ec26fe3
Igor Djachenko <i.s.djachenko@gmail.com>
2026-07-17 06:34:58 +0700

    fix: route compass gestures through gestureLayer

--- app/Cullen/Modules/PhotoViewer/PhotoViewerViewModel.swift
+++ app/Cullen/Modules/PhotoViewer/PhotoViewerViewModel.swift
@@ -156,31 +156,23 @@ final class PhotoViewerViewModel: ObservableObject {
 
 
 extension PhotoViewerViewModel {
-    func handle(recognizer: UIPanGestureRecognizer) {
-        let translation = recognizer.translation(in: recognizer.view)
-        let velocity = recognizer.velocity(in: recognizer.view)
-
-        switch recognizer.state {
-        case .changed:
-            if let (angle, progress) = swipeHandler.track(translation: translation),
-               let direction = SwipeDirection(angle: angle) {
-                compassViewModel = SwipeCompassViewModel(activeDirection: direction, progress: progress)
-            } else {
-                compassViewModel = nil
-            }
-
-        case .ended, .cancelled:
-            if let angle = swipeHandler.evaluate(translation: translation, velocity: velocity),
-               let direction = SwipeDirection(angle: angle) {
-                commitSwipe(direction)
-            } else {
-                cancelSwipe()
-            }
+    func trackSwipe(translation: CGPoint) {
+        if let (angle, progress) = swipeHandler.track(translation: translation),
+           let direction = SwipeDirection(angle: angle) {
+            compassViewModel = SwipeCompassViewModel(activeDirection: direction, progress: progress)
+        } else {
             compassViewModel = nil
-
-        default:
-            break
         }
+    }
+
+    func endSwipe(translation: CGPoint, velocity: CGPoint) {
+        if let angle = swipeHandler.evaluate(translation: translation, velocity: velocity),
+           let direction = SwipeDirection(angle: angle) {
+            commitSwipe(direction)
+        } else {
+            cancelSwipe()
+        }
+        compassViewModel = nil
     }
 }
 
